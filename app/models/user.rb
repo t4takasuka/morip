dbb12a45b5c858fc639a764b63597247e4f066e0
t4takasuka <t4botks@gmail.com>
2020-07-06 16:02:22 +0900

    add-user-function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, length: { minimum: 6 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 6 }, allow_blank: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
 end
