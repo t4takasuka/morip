3e98d585c6a5f42e9eb7bcaa6fdc3a8defd1ceab
t4takasuka <t4botks@gmail.com>
2020-07-16 16:58:37 +0900

    add-relationship,user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { minimum: 6 }, allow_blank: true
   validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
