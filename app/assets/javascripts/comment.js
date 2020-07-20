0b89e7e75b56ae26f25360fa5823bc0729e0a8b1
t4takasuka <t4botks@gmail.com>
2020-07-20 18:25:47 +0900

    add-ajax-comment

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,32 @@
+$(function(){
+  function buildHTML(comment){
+    var html = `<p>
+                  <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    // console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  });
+});
