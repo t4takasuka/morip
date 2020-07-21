07f897278785cc07ac6c3d6d909055976468cec5
t4takasuka <t4botks@gmail.com>
2020-07-21 19:16:07 +0900

    add : comment　削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    comment.destroy
+    post = Post.find(params[:post_id])
+    redirect_to post_path(post.id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
