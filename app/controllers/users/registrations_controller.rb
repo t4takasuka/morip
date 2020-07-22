815461da9406f58a1c27c3859b8d4e33da415a0c
t4takasuka <t4botks@gmail.com>
2020-07-22 19:17:32 +0900

    edit テストユーザー登録機能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -48,6 +48,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :image])
+  end
+
+  def after_update_path_for(resource)
+    user_path(current_user)
   end
 
   # The path used after sign up.
