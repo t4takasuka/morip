01755f53125cb50d0efdbc8a46797d7be224c83b
t4takasuka <t4botks@gmail.com>
2020-07-23 15:48:53 +0900

    add ハッシュ削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
   mount_uploader :image, ImageUploader
   
